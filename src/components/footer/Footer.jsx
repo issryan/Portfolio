5699b1e0d99b51c421907d9bf954ffe00e5ff613
Ryan <90571282+issryan@users.noreply.github.com>
2024-01-23 00:09:14 -0500

    modify: footer links css changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,16 +8,16 @@ const Footer = () => {
     <footer>
         <a href="#home" className='footer__logo'>RYAN ARAFEH</a>
         <ul className="permalinks">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#experience">Experience</a></li>
-            <li><a href="#myprojects">My Projects</a></li>
-            <li><a href="#contact">Contact Me</a></li>
+            <li><a href="#home" className='footer-link'>Home</a></li>
+            <li><a href="#about" className='footer-link'>About</a></li>
+            <li><a href="#experience" className='footer-link'>Experience</a></li>
+            <li><a href="#myprojects" className='footer-link'>My Projects</a></li>
+            <li><a href="#contact" className='footer-link'>Contact Me</a></li>
         </ul>
 
         <div className="footer__socials">
-        <a href="https://www.linkedin.com/in/ryanarafeh" target="_blank" rel='noreferrer'><BsLinkedin/></a>
-        <a href="https://github.com/issryan" target="_blank" rel='noreferrer'><BsGithub/></a>
+        <a href="https://www.linkedin.com/in/ryanarafeh" className='footer-icon'target="_blank" rel='noreferrer'><BsLinkedin/></a>
+        <a href="https://github.com/issryan" className='footer-icon' target="_blank" rel='noreferrer'><BsGithub/></a>
         </div>
 
         <div className="footer__copyright">
