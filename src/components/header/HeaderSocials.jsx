29bf820e380cf4702a582e363ab92c66f9d28ffc
Ryan <90571282+issryan@users.noreply.github.com>
2024-01-29 08:52:00 -0500

    fixed css icon hover effect

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {BsGithub} from 'react-icons/bs'
+import './header.css'
 
 const HeaderSocials = () => {
   return (
     
       <>
-        <a href="https://www.linkedin.com/in/ryanarafeh" target="_blank" rel='noreferrer'><BsLinkedin/></a>
-        <a href="https://github.com/issryan" target="_blank" rel='noreferrer'><BsGithub/></a>
+        <a href="https://www.linkedin.com/in/ryanarafeh" className='header-icon' target="_blank" rel='noreferrer'><BsLinkedin/></a>
+        <a href="https://github.com/issryan" className='header-icon'target="_blank" rel='noreferrer'><BsGithub/></a>
       </>
   )
 }
