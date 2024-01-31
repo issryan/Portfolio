bbb32ebe698046b19d07e88a077c2e0557127c7f
Ryan <90571282+issryan@users.noreply.github.com>
2024-01-31 14:27:08 -0500

    Update Myprojects.jsx

--- src/components/myprojects/Myprojects.jsx
+++ src/components/myprojects/Myprojects.jsx
@@ -47,7 +47,7 @@ const Myprojects = () => {
             <h2 class="experience-sub-title project-title">Alphard Survey</h2>
             <div class="btn-container">
               <a href="https://github.com/Alphard-Survey/Alphard-Survey-Base-Model" className='btn btn-color-2' target="_blank" rel='noreferrer'>Github</a>
-              <a href="https://ryanarafeh.netlify.app/"className='btn btn-color-1' target="_blank" rel='noreferrer'>Live Demo</a>
+              
             </div>
           </div>
           <div class="details-container color-container">
