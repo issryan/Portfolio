c95d143a81d8303b4ff2247fed579144fefe6d52
Ryan <90571282+issryan@users.noreply.github.com>
2024-05-15 14:25:25 -0400

    added more projects and updated styling for containers

--- src/components/myprojects/Myprojects.jsx
+++ src/components/myprojects/Myprojects.jsx
@@ -3,6 +3,8 @@ import './myprojects.css'
 import IMG1 from '../../assets/portfolio.png'
 import IMG2 from '../../assets/alphard_survey.png'
 import IMG3 from '../../assets/shift_logo.png'
+import IMG4 from '../../assets/breedfinder.png'
+
 
 
 const Myprojects = () => {
@@ -17,11 +19,38 @@ const Myprojects = () => {
   }
 
   return (
-    <section id="projects">
-      <p className="section__text__p1">Browse My Recent</p>
+    <section id="projects" className="my-projects-section">
+    <p className="section__text__p1">Browse My Recent</p>
       <h1 className="title">Projects</h1>
       <div className="experience-details-container">
         <div className="about-containers">
+        <div className="details-container color-container">
+            <div className="article-container">
+              <img
+                src={IMG3}
+                alt="Project 3"
+                className="project-img"
+              />
+            </div>
+            <h2 class="experience-sub-title project-title">Shift Happens</h2>
+            <div class="btn-container">
+            <a href="https://github.com/issryan/ShiftHappens" className='btn btn-color-2' target="_blank" rel='noreferrer'>Github</a>
+            <button onClick={shiftAlert} className='btn btn-color-1' target="_blank" rel='noreferrer'>In Progress...</button>
+            </div>
+          </div>
+          <div className="details-container color-container">
+            <div className="article-container">
+              <img
+                src={IMG4}
+                alt="Project 1"
+                className="project-img"
+              />
+            </div>
+            <h2 className="experience-sub-title project-title">Breed Finder</h2>
+            <div className="btn-container">
+              <a href="https://github.com/issryan/BreedFinder" className='btn btn-color-2' target="_blank" rel='noreferrer'>Github</a>
+            </div>
+          </div>
           <div className="details-container color-container">
             <div className="article-container">
               <img
@@ -48,20 +77,6 @@ const Myprojects = () => {
             <div className="btn-container">
               <a href="https://github.com/Alphard-Survey/Alphard-Survey-Base-Model" className='btn btn-color-2' target="_blank" rel='noreferrer'>Github</a>
               
-            </div>
-          </div>
-          <div className="details-container color-container">
-            <div className="article-container">
-              <img
-                src={IMG3}
-                alt="Project 3"
-                className="project-img"
-              />
-            </div>
-            <h2 class="experience-sub-title project-title">Shift Happens</h2>
-            <div class="btn-container">
-            <a href="https://github.com/issryan/ShiftHappens" className='btn btn-color-2' target="_blank" rel='noreferrer'>Github</a>
-            <button onClick={shiftAlert} className='btn btn-color-1' target="_blank" rel='noreferrer'>In Progress...</button>
             </div>
           </div>
         </div>
