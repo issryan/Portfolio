7cdedac744845d445602dd8a5069c54e3496cf15
Ryan <90571282+issryan@users.noreply.github.com>
2024-05-31 23:06:30 -0400

    added logo and services section to navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,7 +5,9 @@ import { BiUser } from 'react-icons/bi'
 import { FaLaptopCode } from 'react-icons/fa'
 import { GoChecklist } from 'react-icons/go'
 import { BiMessageSquareDetail } from 'react-icons/bi'
+import { MdWork } from 'react-icons/md';
 import { useState } from 'react'
+import logo from '../../assets/logo.png';
 
 
 
@@ -20,6 +22,10 @@ const Nav = () => {
       <a href="#home" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BiUser /></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><FaLaptopCode /></a>
+      <div className="nav-logo">
+        <img src={logo} alt="Logo" />
+      </div>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><MdWork /></a>
       <a href="#projects" onClick={() => setActiveNav('#myprojects')} className={activeNav === '#myprojects' ? 'active' : ''}><GoChecklist /></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></a>
 
