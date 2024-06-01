d27954d45289e58044a1a0d3b9e4d3b36db2b5b1
Ryan <90571282+issryan@users.noreply.github.com>
2024-05-31 22:41:15 -0400

    updated services styling

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -6,31 +6,31 @@ import photographyImage from '../../assets/photography.png';
 
 const Services = () => {
   return (
-    <section id="services" className="my-projects-section">
+    <section id="services" className="my-services-section">
       <p className="section__text__p1">What I Offer</p>
       <h1 className="title">Services</h1>
-      <div className="experience-details-container">
-        <div className="about-containers">
-          <div className="details-container color-container">
+      <div className="services-details-container">
+        <div className="services-containers">
+          <div className="services-details color-container">
             <div className="article-container">
-              <img src={webDevImage} alt="Web Development" className="project-img" />
+              <img src={webDevImage} alt="Web Development" className="service-img" />
             </div>
-            <h2 className="experience-sub-title project-title">Web Development</h2>
-            <p>I build responsive and dynamic websites using the latest technologies.</p>
+            <h2 className="service-title">Web Development</h2>
+            <p className='service-subtitle'>I build responsive and dynamic websites using the latest technologies.</p>
           </div>
-          <div className="details-container color-container">
+          <div className="services-details color-container">
             <div className="article-container">
-              <img src={uiuxImage} alt="UI/UX Design" className="project-img" />
+              <img src={uiuxImage} alt="UI/UX Design" className="service-img" />
             </div>
-            <h2 className="experience-sub-title project-title">UI/UX Design</h2>
-            <p>I design user-centered interfaces and create seamless user experiences.</p>
+            <h2 className="experience-sub-title service-title">UI/UX Design</h2>
+            <p className='service-subtitle'>I design user-centered interfaces and create seamless user experiences.</p>
           </div>
-          <div className="details-container color-container">
+          <div className="services-details color-container">
             <div className="article-container">
-              <img src={photographyImage} alt="Photography" className="project-img" />
+              <img src={photographyImage} alt="Photography" className="service-img" />
             </div>
-            <h2 className="experience-sub-title project-title">Photography</h2>
-            <p>I capture moments and create stunning visual content.</p>
+            <h2 className="experience-sub-title service-title">Photography</h2>
+            <p className='service-subtitle'>I capture moments and create stunning visual content.</p>
           </div>
         </div>
       </div>
